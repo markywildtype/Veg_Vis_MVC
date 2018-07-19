416f9f74a0fdc5b2c720272cd9b7c8d94e5b0f01
Mark Blanford <markytorture@hotmail.com>
2018-07-19 13:14:01 +0100

    JavaScript added to dynamically adjust content on listings pages

---
+++ wwwroot/js/main.js
@@ -0,0 +1,39 @@
+﻿const app = function() {
+
+    const allButton = document.querySelector('#all-button');
+    const accommodationButton = document.querySelector('#accommodation-button');
+    const eatingButton = document.querySelector('#eating-button');
+
+    allButton.addEventListener('click', buttonClicked);
+    accommodationButton.addEventListener('click', buttonClicked);
+    eatingButton.addEventListener('click', buttonClicked);
+
+}
+
+
+const buttonClicked = function() {
+    const accomElements = document.querySelector('.accommodation');
+    const eatingElements = document.querySelector('.eating');
+
+    switch (this.id) {
+        case 'all-button':
+            accomElements.style.display = 'block';
+            eatingElements.style.display = 'block';
+            break;
+        case 'accommodation-button':
+            accomElements.style.display = 'block';
+            eatingElements.style.display = 'none';
+            break;
+        case 'eating-button':
+            accomElements.style.display = 'none';
+            eatingElements.style.display = 'block';
+            break;
+        default:
+            break;
+    }
+
+}
+
+
+
+document.addEventListener('DOMContentLoaded', app);
